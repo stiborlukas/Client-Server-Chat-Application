cb0e35c01032e1cbf4fd7526bb17bac375e3308a
stibolu20 <stibolu20@sps-prosek.cz>
2022-03-08 15:54:58 +0100

    JDU SKOCIT Z MOSTU

--- server.py
+++ server.py
@@ -3,7 +3,9 @@ import socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
+# host = "127.0.0.1"
 port = 2205
+
 
 serversocket.bind((host, port))
 
@@ -11,4 +13,7 @@ while True:
     serversocket.listen()
     clientsocket, addr = serversocket.accept()
     clientsocket.sendall(b'Hello world!')
+    dataFromServer = clientsocket.recv(1024)
+    print(dataFromServer)
+    # clientsocket.sendall(dataFromServer)
     clientsocket.close()
