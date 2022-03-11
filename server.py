e0387172023dbcc7305ec6a530f25a1be79530c6
lukxyx <stiborlukas@gmail.com>
2022-03-11 17:35:59 +0100

    server hotov

--- server.py
+++ server.py
@@ -18,3 +18,67 @@
 #     print(dataFromServer)
 #     # clientsocket.sendall(dataFromServer)
 #     clientsocket.close()
+import socket
+import threading
+
+host = "127.0.0.1"
+port = 2205
+# port = 9090
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind((host, port))
+
+serversocket.listen()
+
+clients = []
+nicknames = []
+
+
+def broadcast(msg):
+    for client in clients:
+        client.send(msg)
+
+
+def handle(client):
+    while True:
+        try:
+            msg = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]} says {msg}")
+            broadcast(msg)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nicknames = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+
+def receive():
+    while True:
+        client, address = serversocket.accept()
+        print(f"connected with {str(address)}!")
+
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"nickname of client is {nickname}")
+        broadcast(f"{nickname} connected to the server!".encode('utf-8'))
+        client.send("connected to the server".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print(f"server running")
+receive()
+
+
+
+
+
+
+
