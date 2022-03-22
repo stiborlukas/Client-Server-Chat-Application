3abc4c47beefeb5e1007f82e4ddcad5eb29544d0
stibolu20 <stibolu20@sps-prosek.cz>
2022-03-22 15:10:47 +0100

    ano

--- klient.py
+++ klient.py
@@ -54,66 +54,72 @@ import tkinter.scrolledtext
 from tkinter import simpledialog
 
 
-def gui_loop():
-    win = tk.Tk()
-
-    win.configure(bg="lightgray")
-
-    chat_label = tk.Label(win, text="CHat:", bg="lightgray")
-    chat_label.pack(padx=20, pady=5)
-
-    textarea = tk.scrolledtext.ScrolledText(win)
-    textarea.pack(padx=20, pady=5)
-    textarea.config(state='disabled')
-
-    msg_label = tk.Label(win, text="Message:", bg="lightgray")
-    msg_label.pack(padx=20, pady=5)
-
-    input_area = tk.Text(win, height=3)
-    input_area.pack(padx=20, pady=5)
-
-    def write():
-        message = f"{nickname}: {input_area.get('1.0', 'end')}"
-        s.send(message.encode('utf-8'))
-        input_area.delete('1.0', 'end')
-
-    send_button = tk.Button(win, text="SEND", command=write)
-    send_button.pack(padx=20, pady=5)
-
-    win.mainloop()
+def write():
+    # message = f"{nickname}: {input_area.get('1.0', 'end')}"
+    # print("fd")
+    message = input_area.get('1.0', 'end')
+    s.send(message.encode('utf-8'))
+    input_area.delete('1.0', 'end')
 
 
 def receive():
-    running = True
-    while running:
-        try:
-            message = s.recv(1024)
-            if message == "NICK":
-                s.send(nickname.encode('utf-8'))
-            else:
-                pass
-        except:
-            pass
+    while True:
+            # print("ffff")
+        msg = s.recv(1024)
+        print(msg)
+            # textarea.config(state='enabled')
+        # textarea.insert(END,  msg)
+            # textarea.config(state='disabled')
+            # if message == "NICK":
+            #     print("nick")
+            #     s.send(nickname.encode('utf-8'))
+            # else:
+            #     textarea.insert(END, message)
 
 
-host = "127.0.0.1"
-# port = 2205
-port = 9090
+host = socket.gethostname()
+# host = "127.0.0.1"
+port = 2205
+# port = 9090
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((host, port))
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
 
-msg = tk.Tk()
-msg.withdraw()
+# window = tk.Tk()
+# window.withdraw()
 
-nickname = simpledialog.askstring("nickname", "please choose nickname", parent=msg)
+# nickname = simpledialog.askstring("nickname", "please choose nickname", parent=msg)
 
 # gui_done = False
 # running = True
 
-gui_thread = threading.Thread(target=gui_loop)
-receive_thread = threading.Thread(target=receive)
+# gui_thread = threading.Thread(target=gui_loop)
 
-gui_thread.start()
-receive_thread.start()
+win = tk.Tk()
+
+win.configure(bg="lightgray")
+
+chat_label = tk.Label(win, text="CHat:", bg="lightgray")
+chat_label.pack(padx=20, pady=5)
+
+textarea = tk.scrolledtext.ScrolledText(win)
+textarea.pack(padx=20, pady=5)
+textarea.config(state='disabled')
+
+msg_label = tk.Label(win, text="Message:", bg="lightgray")
+msg_label.pack(padx=20, pady=5)
+
+input_area = tk.Text(win, height=3)
+input_area.pack(padx=20, pady=5)
+
+
+send_button = tk.Button(win, text="SEND", command=write)
+send_button.pack(padx=20, pady=5)
+
+win.mainloop()
+
+
+# gui_thread.start()
--- server.py
+++ server.py
@@ -18,12 +18,15 @@
 #     print(dataFromServer)
 #     # clientsocket.sendall(dataFromServer)
 #     clientsocket.close()
+
+# ----------------------------------------------
 import socket
 import threading
 
-host = "127.0.0.1"
-# port = 2205
-port = 9090
+# host = "127.0.0.1"
+host = socket.gethostname()
+port = 2205
+# port = 9090
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((host, port))
@@ -34,41 +37,24 @@ clients = []
 nicknames = []
 
 
-def broadcast(msg):
-    for client in clients:
-        client.send(msg)
-
+def broadcast(msg, client):
+    # ano = msg
+    # # print("gfhjfgdhj")
+    print(msg)
+    client.send(msg)
+    # client.sendall(msg)
 
 def handle(client):
     while True:
-        try:
-            msg = client.recv(1024)
-            print(f"{nicknames[clients.index(client)]} says {msg}")
-            broadcast(msg)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nicknames = nicknames[index]
-            nicknames.remove(nickname)
-            break
+        msg = client.recv(1024)
+        broadcast(msg, client)
 
 
 def receive():
     while True:
         client, address = serversocket.accept()
         print(f"connected with {str(address)}!")
-
-        client.send("NICK".encode('utf-8'))
-        nickname = client.recv(1024)
-
-        nicknames.append(nickname)
-        clients.append(client)
-
-        print(f"nickname of client is {nickname}")
-        broadcast(f"{nickname} connected to the server!".encode('utf-8'))
         client.send("connected to the server".encode('utf-8'))
-
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
