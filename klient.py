2269942fc543170d6ccbd7032293f87beddc82b2
stibolu20 <stibolu20@sps-prosek.cz>
2022-03-29 15:52:26 +0200

    ja uz nemuzu prosim zachrante me nekdo

--- klient.py
+++ klient.py
@@ -1,117 +1,53 @@
-# -------------------------------------------skola
-# import socket
-# import tkinter as tk
-#
-# window = tk.Tk()
-# entry = tk.Entry(width=22).pack()
-#
-#
-# def ano():
-#     text = f"{entry.get()}"
-#     entry.delete(0,"end")
-#     entry.insert(0, text)
-#
-#
-# message = tk.Button(text="ano", command=ano).pack()
-#
-# canvas= tk.Canvas(window, width= 1000, height= 750)
-#
-# # canvas.create_text(300, 50, text="HELLO WORLD")
-# # canvas.pack()
-#
-#
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-# # host = socket.gethostname()
-# host = "127.0.0.1"
-# port = 2205
-#
-# s.connect((host, port))
-# msg = s.recv(1024)
-# print(msg.decode('ascii'))
-#
-# # window.insert(0, "ano")
-# zprava = "server: ",  msg
-# canvas.create_text(300, 50, text=zprava)
-# canvas.pack()
-#
-# data = "Heskfnjdnfsj!";
-#
-# s.send(data.encode());
-# s.close()
-#
-#
-#
-#
-#
-#
-#
-# window.mainloop()
 import socket
 import threading
 import tkinter as tk
 import tkinter.scrolledtext
 from tkinter import simpledialog
+import sys
 
 
+# odesilani zprav
 def write():
-    # message = f"{nickname}: {input_area.get('1.0', 'end')}"
-    # print("fd")
     message = input_area.get('1.0', 'end')
     s.send(message.encode('utf-8'))
     input_area.delete('1.0', 'end')
 
 
-# def ask_name():
-#     window = tk.Tk()
-#     window.withdraw()
-#
-#     nickname = simpledialog.askstring("nickname", "please choose nickname", parent=window)
-#     message = f"{nickname}: {input_area.get('1.0', 'end')}"
-#     s.send(message.encode('utf-8'))
-
+# prijimani zprav
 def receive():
     while True:
-
         msg = s.recv(1024).decode('utf-8')
         print(msg)
         try:
             if msg == "NICK":
-                # ask_name()
                 message = f"{nickname}: {input_area.get('1.0', 'end')}"
                 s.send(message.encode('utf-8'))
+            else:
+
+                textarea.config(state='normal')
+                textarea.insert("end", msg)
+                textarea.config(state='disabled')
         except:
-            # msg = s.recv(1024)
-            print(msg)
-            textarea.config(state='normal')
-            textarea.insert("end", msg)
-            textarea.config(state='disabled')
-            # if message == "NICK":
-            #     print("nick")
-            #     s.send(nickname.encode('utf-8'))
-            # else:
-            #     textarea.insert(END, message)
+            s.close()
+            sys.exit()
 
 
-host = socket.gethostname()
-# host = "127.0.0.1"
-port = 2205
-# port = 9090
+# nastaveni promennych
+host = "127.0.0.1"
+# host = socket.gethostname()
+# port = 2205
+port = 9090
 
+# nastaveni pripojeni
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.connect((host, port))
 
+# okno ve kterem se zeptam na jmeno
 window = tk.Tk()
 window.withdraw()
-
 nickname = simpledialog.askstring("nickname", "please choose nickname", parent=window)
 
-# gui_done = False
-# running = True
-
-# gui_thread = threading.Thread(target=gui_loop)
-
+# -------------------------------------tkinter
 win = tk.Tk()
 
 win.configure(bg="lightgray")
@@ -129,13 +65,10 @@ msg_label.pack(padx=20, pady=5)
 input_area = tk.Text(win, height=3)
 input_area.pack(padx=20, pady=5)
 
-
+# win.bind('<Return>', write)
 send_button = tk.Button(win, text="SEND", command=write)
 send_button.pack(padx=20, pady=5)
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 win.mainloop()
-
-
-# gui_thread.start()
--- server.py
+++ server.py
@@ -1,78 +1,111 @@
-# ----------------------------------------skola
-# import socket
-#
-# serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-# # host = socket.gethostname()
-# host = "127.0.0.1"
-# port = 2205
-#
-#
-# serversocket.bind((host, port))
-#
-# while True:
-#     serversocket.listen()
-#     clientsocket, addr = serversocket.accept()
-#     clientsocket.sendall(b'Hello world!')
-#     dataFromServer = clientsocket.recv(1024)
-#     print(dataFromServer)
-#     # clientsocket.sendall(dataFromServer)
-#     clientsocket.close()
-
-# ----------------------------------------------
 import socket
 import threading
+import logging
 
-# host = "127.0.0.1"
-host = socket.gethostname()
-port = 2205
-# port = 9090
 
+# odesilani vsem klientum
+def broadcast(msg, client, nickname):
+    # zbaveni se \n
+    nick = nickname.strip()
+    msg_temp = msg.strip()
+
+    # zprava kterou poslu
+    message = f"{nick} {msg_temp}\n"
+    message = message.encode('utf-8')
+
+    # poslani vsem klientum
+    for client in clients:
+        client.send(message)
+
+
+# prijimani zprav od klientu
+def handle(client, nickname, clients):
+    while True:
+        try:
+
+            msg = client.recv(1024).decode('utf-8')
+            print(msg)
+            if msg:
+                broadcast(msg, client, nickname)
+        # except Exception as e:
+        #     logging.error(e)
+        except ConnectionResetError:
+            print(f"{clients_connected[client_socket][0]} disconnected")
+
+            for client in clients_connected:
+                if client != client_socket:
+                    client.send('notification'.encode())
+
+                    data = pickle.dumps({'message': f"{clients_connected[client_socket][0]} left the chat",
+                                         'id': clients_connected[client_socket][1], 'n_type': 'left'})
+
+                    data_length_bytes = struct.pack('i', len(data))
+                    client.send(data_length_bytes)
+
+                    client.send(data)
+
+            del clients_data[clients_connected[client_socket][1]]
+            del clients_connected[client_socket]
+            client_socket.close()
+            break
+        except ConnectionAbortedError:
+            print(f"{clients_connected[client_socket][0]} disconnected unexpectedly.")
+
+            for client in clients_connected:
+                if client != client_socket:
+                    client.send('notification'.encode())
+                    data = pickle.dumps({'message': f"{clients_connected[client_socket][0]} left the chat",
+                                         'id': clients_connected[client_socket][1], 'n_type': 'left'})
+                    data_length_bytes = struct.pack('i', len(data))
+                    client.send(data_length_bytes)
+                    client.send(data)
+
+            del clients_data[clients_connected[client_socket][1]]
+            del clients_connected[client_socket]
+            client_socket.close()
+            break
+
+
+# zakladni pripojeni
+def receive():
+    while True:
+        try:
+            client, address = serversocket.accept()
+
+            # zeptat se na jmeno, pridat ho do listu
+            client.send("NICK".encode('utf-8'))
+            nickname = client.recv(1024).decode("utf-8")
+            clients.append(client)
+
+            print(f"connected with {str(address)}!")
+
+            # dat mu vedet ze pripojeny, spustit prijimani zprav
+            client.send("connected to the server\n".encode('utf-8'))
+            thread = threading.Thread(target=handle, args=(client, nickname, clients))
+            thread.start()
+        except Exception as e:
+            logging.error(e)
+
+
+# logging setup
+logging.basicConfig(filename='server_logs.log', filemode='w', format='%(asctime)s : %(message)s',datefmt='[%d/%m/%Y] %H:%M:%S' ,level=logging.DEBUG)
+
+# nastaveni promennych
+host = "127.0.0.1"
+# host = socket.gethostname()
+# port = 2205
+port = 9090
+clients = []
+
+
+# nastaveni pripojeni
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((host, port))
 
 serversocket.listen()
 
-clients = []
-nicknames = []
-
-
-def broadcast(msg, client, nickname):
-    # ano = msg
-    # # print("gfhjfgdhj")
-    # print(msg)
-    message = f"{nickname} says {msg}".encode('utf-8')
-    client.send(message)
-    # client.sendall(msg)
-
-
-def handle(client, nickname):
-    while True:
-        msg = client.recv(1024)
-        if msg:
-            broadcast(msg, client, nickname)
-
-
-def receive():
-    while True:
-        client, address = serversocket.accept()
-
-        client.send("NICK".encode('utf-8'))
-        nickname = client.recv(1024)
-        # print(nickname)
-
-        print(f"connected with {str(address)}!")
-        client.send("connected to the server".encode('utf-8'))
-        thread = threading.Thread(target=handle, args=(client, nickname,))
-        thread.start()
-
 
 print(f"server running")
+
+# spusteni
 receive()
-
-
-
-
-
-
-
